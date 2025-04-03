ff1be219374397b355cc0da2dd498a51f2db065f
Marco <marco@jusid.de>
2025-04-03 10:08:20 +0200

    Add base class for our db actions.

---
+++ classes/TodoDB.php
@@ -0,0 +1,38 @@
+<?php
+
+require_once('./logging.php');
+require_once('./config.php');
+
+/**
+ * Todo list database object.
+ *
+ * Global variable with the object of our TodoDB class.
+ */
+$todoDB = new TodoDB();
+
+class TodoDB {
+    private $connection;
+    private $stmt;
+
+    /**
+     * Contructructor of the TodoDB class.
+     */
+    public function __construct() {
+        global $host, $db, $user, $pass;
+        try {
+            $this->connection = new PDO(
+                "mysql:host=$host;dbname=$db;",
+                $user,
+                $pass
+            );
+            $this->connection->setAttribute(
+                PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+            write_log("HINT", "connection established");
+        } catch (Exception $e) {
+            write_log("ERROR", $e->getMessage());
+        }
+    }
+
+}
+
+?>
--- todo-api.php
+++ todo-api.php
@@ -2,12 +2,8 @@
 header('Content-Type: application/json');
 
 require_once('./logging.php');
-
-$host = '127.0.0.1';
-$db = 'todo_list';
-$user = 'j23d';
-$pass = 'beep';
-$charset = 'utf8mb4';
+require_once('./config.php');
+require_once('./classes/TodoDB.php');
 
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
 $options = [
